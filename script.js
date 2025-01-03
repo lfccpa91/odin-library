fc545819d5aeaeb4dc7708e499987b40c93ed448
lfccpa91 <tyler.searle@hotmail.com>
2025-01-03 02:39:01 -0700

    Created basic skeleton in HTML, and began creating the javascript functions needed to create books and add to library

---
+++ script.js
@@ -0,0 +1,13 @@
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        return `${title} by ${author}, ${pages}, ${read ? "read" : "not read yet"}.`
+    }
+}
+
+function addBookToLibrary(book) {
+    return null
+}
